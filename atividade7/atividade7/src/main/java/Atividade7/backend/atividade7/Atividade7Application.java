75e8d0da2406f65d62cc24b1a4896d8168afce29
Suyane Santos <132683758+suyane924@users.noreply.github.com>
2024-05-12 16:46:53 -0300

    Exercício 7 e 8

---
+++ atividade7/atividade7/src/main/java/Atividade7/backend/atividade7/Atividade7Application.java
@@ -0,0 +1,13 @@
+package Atividade7.backend.atividade7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade7Application.class, args);
+	}
+
+}
